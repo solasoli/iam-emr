2f0be283d554b3059061dab3e1c182fa72bbf430
unknown <azolaakbar@gmail.com>
2018-12-28 03:33:13 +0700

    1

---
+++ app/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+    <Page actionBarHidden="true">
+        
+
+       
+    </Page>
+</template>
+
+<script>
+    import Login from "./Login";
+
+    export default {
+        data() {
+            return {
+                
+            };
+        },
+        methods: {
+            logout() {
+                this.$backendService.logout();
+                this.$navigateTo(Login, {
+                    clearHistory: true
+                });
+            }
+        }
+    };
+</script>
+
+<style>
+</style>
